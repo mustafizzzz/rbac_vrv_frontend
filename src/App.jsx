a987015a077fc1fa1c7a485ca6c722b0e0c6a10c
Mustafiz Ansari <mustafizansari35@gmail.com>
2024-12-06 02:28:11 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="w-full h-screen bg-slate-800">
+        <h1 className="bg-white">test page 12</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
